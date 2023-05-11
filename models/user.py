65f68c267236b9d41de22d142cfcab9902f0734d
Kennedy Ekanem <kennedyekanem@gmail.com>
2023-05-11 14:23:43 +0100

    modified user_class

--- models/user.py
+++ models/user.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
+from models.place import Place
+from sqlalchemy.ext.declarative import declarative_base
+from models.review import Review
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """
+        This class defines, a user by various attributes SQLALCHEMY mapping
+        This is the mapping mechanism(Instrumentation, of 'class' to 'table')
+    """
+    __tablename__ = 'users'
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship('Place', cascade='delete, all, delete-orphan',
+                          backref='user')
+    reviews = relationship('Review', cascade='all, delete, delete-orphan',
+                           backref='user')
