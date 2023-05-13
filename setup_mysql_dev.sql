46c6abf7ee2bc9b2cbb9543438b29c0bec5d5722
Kennedy Ekanem <kennedyekanem@gmail.com>
2023-05-13 18:42:51 +0100

    Update params | models

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,12 +1,10 @@
--- this script prepares a MySQL server for the project
--- create project developement database with the name : hbnb_dev_db
+-- prepare Mysql server
+-- create a new database hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
--- creating new user named : hbnb_dev with all privileges on the db hbnb_dev_db
--- with the password : hbnb_dev_pwd if it dosen't exist
+-- create a new user hbnhb_dev identified by password hbnb_dev_pwd
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
--- granting all privileges to the new user
+-- grant all privileges to hbnb_dev_db database and all tables
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
--- granting the SELECT privilege for the user hbnb_dev in the db performance_schema
+-- grant select permission to performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,12 +1,10 @@
--- this script prepares a MySQL server for the project
--- create project testing database with the name : hbnb_test_db
+-- setup my sql server
+-- create new database hbnb_test_db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
--- creating new user named : hbnb_test with all privileges on the db hbnb_test_db
--- with the password : hbnb_test_pwd if it dosen't exist
+-- create new user hbnb_test identified by password hbnb_test_pwd
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
--- granting the SELECT privilege for the user hbnb_test on the db performance_schema
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES;
--- granting all privileges to the new user on hbnb_test_db
+-- grant all privileges to database hbnb_test_db and all tables
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+-- grant select permission to performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
