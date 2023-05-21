16a2f3e38ea260378524ed23fa36527b421c7930
Kennedy Ekanem <kennedyekanem@gmail.com>
2023-05-21 23:15:28 +0100

    Hello world flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""new flask application
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    Displays 'Hello HBNB!'
+"""
+
 from flask import Flask
-"""new flask application"""
+
 app = Flask(__name__)
 
 
