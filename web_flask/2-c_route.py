c7d67ac78343769cde3a8c74a940534a22d7d5fe
Kennedy Ekanem <kennedyekanem@gmail.com>
2023-05-22 00:20:39 +0100

    Updated c

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,33 +1,35 @@
 #!/usr/bin/python3
-"""This is a flask application
-The application listens on 0.0.0.0, port 5000.
-Routes:   
-/: display “Hello HBNB!”
-/hbnb: display “HBNB”
-/c/<text>: display “C ” followed by the value of the text variable (replace underscore _ symbols with a space )
+"""Starts a Flask web application.
 
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by the value of <text>.
 """
 from flask import Flask
+
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def display():
-    """display hello HBNB!"""
-    return ("hello HBNB!")
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'."""
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
-def display_hbnb():
-    """display hello HBNB"""
-    return ("HBNB")
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays 'HBNB'."""
+    return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    """display c and text"""
-    return ("C %s" % (text.replace('_', ' ')))
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Displays 'C' followed by the value of <text>."""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0")
