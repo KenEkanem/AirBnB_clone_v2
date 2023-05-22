0147fbdaa4963724b93e3b5ef6bafd3101b1f0ce
Kennedy Ekanem <kennedyekanem@gmail.com>
2023-05-22 12:24:21 +0100

    removed blank lines

--- tests/test_console.py
+++ tests/test_console.py
@@ -17,10 +17,9 @@ from models.amenity import Amenity
 from models.review import Review
 
 
-
 class TestCommand(unittest.TestCase):
     """Class that tests the console"""
-    
+
     def setUp(self):
         """Function empties file.json"""
         FileStorage._FileStorage__objects = {}
@@ -68,64 +67,4 @@ class TestCommand(unittest.TestCase):
         pi = storage.all()[f'City.{result}'].pi
         self.assertEqual(pi, '3.14')
         with patch('sys.stdout', new=io.StringIO()) as f:
-            HBNBCommand().onecmd('create Amenity johnny="bravo"'
-                                 ' number="7" pi="3.14"')
-        result = f.getvalue().strip()
-        self.assertRegex(result, opt)
-        johnny = storage.all()[f'Amenity.{result}'].johnny
-        self.assertEqual(johnny, "bravo")
-        number = storage.all()[f'Amenity.{result}'].number
-        self.assertEqual(number, '7')
-        pi = storage.all()[f'Amenity.{result}'].pi
-        self.assertEqual(pi, '3.14')
-        with patch('sys.stdout', new=io.StringIO()) as f:
-            HBNBCommand().onecmd('create Place johnny="bravo"'
-                                 ' number="7" pi="3.14"')
-        result = f.getvalue().strip()
-        self.assertRegex(result, opt)
-        johnny = storage.all()[f'Place.{result}'].johnny
-        self.assertEqual(johnny, "bravo")
-        number = storage.all()[f'Place.{result}'].number
-        self.assertEqual(number, '7')
-        pi = storage.all()[f'Place.{result}'].pi
-        self.assertEqual(pi, '3.14')
-        with patch('sys.stdout', new=io.StringIO()) as f:
-            HBNBCommand().onecmd('create Review johnny="bravo"'
-                                 ' number="7" pi="3.14"')
-        result = f.getvalue().strip()
-        self.assertRegex(result, opt)
-        johnny = storage.all()[f'Review.{result}'].johnny
-        self.assertEqual(johnny, "bravo")
-        number = storage.all()[f'Review.{result}'].number
-        self.assertEqual(number, '7')
-        pi = storage.all()[f'Review.{result}'].pi
-        self.assertEqual(pi, '3.14')
-        with patch('sys.stdout', new=io.StringIO()) as f:
-            HBNBCommand().onecmd('create')
-        opt = '** class name missing **\n'
-        self.assertEqual(f.getvalue(), opt)
-        with patch('sys.stdout', new=io.StringIO()) as f:
-            HBNBCommand().onecmd('create NotClass')
-        opt = '** class doesn\'t exist **\n'
-        self.assertEqual(f.getvalue(), opt)
-
-    def testPycodeStyle(self):
-        """Pycodestyle test for console.py"""
-        style = pycodestyle.StyleGuide(quiet=True)
-        p = style.check_files(['console.py'])
-        self.assertEqual(p.total_errors, 0, "fix pep8")
-
-    def test_doc_console(self):
-        self.assertIsNotNone(HBNBCommand.__doc__)
-        self.assertIsNotNone(HBNBCommand.do_all.__doc__)
-        self.assertIsNotNone(HBNBCommand.do_create.__doc__)
-        self.assertIsNotNone(HBNBCommand.do_destroy.__doc__)
-        self.assertIsNotNone(HBNBCommand.do_quit.__doc__)
-        self.assertIsNotNone(HBNBCommand.do_EOF.__doc__)
-        self.assertIsNotNone(HBNBCommand.do_count.__doc__)
-        self.assertIsNotNone(HBNBCommand.do_update.__doc__)
-        self.assertIsNotNone(HBNBCommand.emptyline.__doc__)
-
-
-if __name__ == '__main__':
-    unittest.main()
+            HBNBCommand().onecmd('create Amenity
